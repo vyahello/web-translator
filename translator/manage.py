ecdd348dc7019b9cb615290cb3146f3ca77a71a6
Volodymyr Yahello <vyahello@gmail.com>
2022-03-28 13:54:35 +0300

    Create initial blog app

--- translator/manage.py
+++ translator/manage.py
@@ -9,6 +9,7 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'translator.settings')
     try:
         from django.core.management import execute_from_command_line
+
         execute_from_command_line(sys.argv)
     except ImportError as exc:
         raise ImportError(
